325f717f54f9365097905f9758e2d2720a046e62
kevindna <kevinand@berkeley.edu>
2023-10-15 06:22:25 +0000

    all testbench collateral

--- hdk/cl/examples/cl_dram_dma/verif/scripts/waves.tcl
+++ hdk/cl/examples/cl_dram_dma/verif/scripts/waves.tcl
@@ -16,7 +16,9 @@
 set curr_wave [current_wave_config]
 if { [string length $curr_wave] == 0 } {
   if { [llength [get_objects]] > 0} {
-    add_wave /
+    current_scope /tb/card/fpga/CL
+    add_wave -r CL_PCIM_MSTR 
+    add_wave -r CL_OCL_SLV
     set_property needs_save false [current_wave_config]
   } else {
      send_msg_id Add_Wave-1 WARNING "No top level signals found. Simulator will start without a wave window. If you want to open a wave window go to 'File->New Waveform Configuration' or type 'create_wave_config' in the TCL console."
